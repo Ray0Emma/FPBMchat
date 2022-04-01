b052fd576aee939d4a70b59c96b810a8654ec5be
Ray0Emma <ahmadifarah22@gmail.com>
2022-04-01 10:16:06 +0000

    fix: added splash screen

---
+++ app/src/main/java/com/example/tp7_asyntask/SplashScreen.java
@@ -0,0 +1,20 @@
+package com.example.tp7_asyntask;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, CreateGroup.class);
+        startActivity(intent);
+        finish();
+    }
+}
